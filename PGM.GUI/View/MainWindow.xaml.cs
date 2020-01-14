6300cddb0ed657e154157b3f1c64782349cb95e1
Yoann TERUEL <yoann.teruel@gmail.fr>
2020-01-14 15:27:32 +0100

    IHM : prepare the implementation of actions in interface

--- PGM.GUI/View/MainWindow.xaml.cs
+++ PGM.GUI/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PGM.GUI
+namespace PGM.GUI.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- PGM.GUI/ViewModel/MainViewModel.cs
+++ PGM.GUI/ViewModel/MainViewModel.cs
@@ -1,13 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Threading.Tasks;
-using GalaSoft.MvvmLight;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using PGM.GUI.Utilities;
+using PGM.Lib;
+using PGM.Lib.Model;
+using PGM.Lib.Model.Issues;
+// ReSharper disable ConvertToNullCoalescingCompoundAssignment
 
 namespace PGM.GUI.ViewModel
 {
     public class MainViewModel : SubViewModelBase
     {
-        public async Task Load()
-        {
+        private readonly GitlabService _gitlabService;
+        private ICommand _activatedCommand;
 
+        public MainViewModel()
+        {
+            GitlabIssues = new ObservableCollection<GitlabIssue>();
+            GroupedIssues = CollectionViewSource.GetDefaultView(GitlabIssues);
+            GroupedIssues.GroupDescriptions.Add(new PropertyGroupDescription(nameof(GitlabIssue.StepType)));
+            _gitlabService = new GitlabService(new PGMSettings(SettingsViewModel));
+        }
+
+        public SettingsViewModel SettingsViewModel { get; } = new SettingsViewModel();
+
+        public ICollectionView GroupedIssues { get; set; }
+
+        public ObservableCollection<GitlabIssue> GitlabIssues { get; set; }
+
+        public ICommand CreateBranchLinkedWithIssueCommand { get; set; }
+
+        public ICommand CreateMergeRequestOnGitlabCommand{ get; set; }
+
+        public ICommand TestActualBranchCommand { get; set; }
+
+        public ICommand ValidateActualBranchCommand { get; set; }
+
+        public ICommand ActivatedCommand =>
+            _activatedCommand ?? (_activatedCommand = CommandFactory.CreateAsync(Activated, CanActivate, nameof(ActivatedCommand), this));
+
+        private bool CanActivate()
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task Activated()
+        {
+            await LoadIssues(true);
+        }
+
+        public bool IsRefreshing { get; set; }
+
+        public async Task LoadIssues(bool refresh = false)
+        {
+            List<GitlabIssue> gitlabIssues = 
+                await await Task.Factory.StartNew(() => _gitlabService.GetAllIssuesOfCurrentSprint());
+            
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                bool previousIsRefreshing = IsRefreshing;
+                IsRefreshing = true;
+                if (!refresh)
+                {
+                    GitlabIssues.Clear();
+                }
+
+                if (!string.IsNullOrEmpty(SettingsViewModel.GitApiKey))
+                {
+                    foreach (GitlabIssue gitlabIssue in gitlabIssues)
+                    {
+                        GitlabIssues.Add(gitlabIssue);
+                    }
+                }
+
+                IsRefreshing = previousIsRefreshing;
+
+                if (refresh)
+                {
+                    GroupedIssues.Refresh();
+                }
+            });
         }
     }
 }
