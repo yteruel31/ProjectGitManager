633697c24815849342d2abf20978ec14d312bc7f
Yoann TERUEL <yoann.teruel@gmail.com>
2020-04-03 09:59:13 +0200

    Add new custom Controls : FolderEntry & ComboBox

---
+++ PGM.GUI/View/Controls/FolderEntry.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Data;
+using System.Windows.Forms;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Control = System.Windows.Controls.Control;
+
+namespace PGM.GUI.View.Controls
+{
+    public class FolderEntry : Control
+    {
+        public string Path
+        {
+            get => (string)GetValue(PathProperty);
+            set => SetValue(PathProperty, value);
+        }
+
+        public static readonly DependencyProperty PathProperty = DependencyProperty.Register("Path", typeof(string),
+            typeof(FolderEntry),
+            new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public ICommand OpenDirectoryDialogCommand => new RelayCommand(OpenDirectoryDialog);
+
+        private void OpenDirectoryDialog()
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                fbd.SelectedPath = Path;
+                fbd.ShowNewFolderButton = true;
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    Path = fbd.SelectedPath;
+                    BindingExpression be = GetBindingExpression(PathProperty);
+                    be?.UpdateSource();
+                }
+            }
+        }
+    }
+}
